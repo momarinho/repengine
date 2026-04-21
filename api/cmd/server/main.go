7a7ea3517c229f49e042d4f05a75043c912fd976
Mateus <mateusomarinho@gmail.com>
2026-04-21 10:38:56 -0300

    chore: initial project scaffold

---
+++ api/cmd/server/main.go
@@ -0,0 +1,13 @@
+package server
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{"status": "ok"})
+	})
+
+	app.Listen(":8080")
+}
